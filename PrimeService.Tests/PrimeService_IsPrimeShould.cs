da6424e97b290fd7449fd3330fad309d80d494a7
nguyenkhanhtung <tungnk0711@gmail.com>
2020-03-05 22:10:46 +0700

    commit

---
+++ PrimeService.Tests/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,24 @@
+using NUnit.Framework;
+
+namespace Prime.Service.Tests
+{
+    [TestFixture]
+    public class PrimeService_IsPrimeShould
+    {
+        private PrimeService _primeService;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _primeService = new PrimeService();
+        }
+
+        [Test]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var result = _primeService.IsPrime(1);
+
+            Assert.IsFalse(result, "1 should not be prime");
+        }
+    }
+}
